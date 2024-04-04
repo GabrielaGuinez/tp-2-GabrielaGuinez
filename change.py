05b73f3f5fda7209a17f8857beba3d9de84d65ad
GabrielaGuinez <162721892+GabrielaGuinez@users.noreply.github.com>
2024-04-04 12:46:45 -0300

    change.py Gabriela

--- change.py
+++ change.py
@@ -1,3 +1,18 @@
 def change():
     expense = 23.75
     money = 100
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido:")
+    print(money)
+    print("")
+    print("Vuelto")
+    print("")
+    print("Pesos:")
+    pesos = money-expense
+    print(int(pesos))
+    print("Centavos:")
+    centavos= (money-expense-int(pesos))*100
+    print(int(centavos))
+
+change()
